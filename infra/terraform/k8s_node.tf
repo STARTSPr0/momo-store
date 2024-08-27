ae3ffde1ff824a0048f43816e3516bbdc909b4c5
STARTS_Pr0 <test@test.ru>
2024-08-27 10:35:09 +0300

    Вынести ноду и сеть в отдельные файлы

---
+++ infra/terraform/k8s_node.tf
@@ -0,0 +1,39 @@
+resource "yandex_kubernetes_node_group" "k8s-node-group" {
+  description = "Node group for Managed Service for Kubernetes cluster"
+  name        = "${var.cluster_name}-node-group"
+  cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
+  version     = var.k8s_version
+
+  scale_policy {
+    auto_scale {
+      initial = var.node_auto_scale_initial
+      min     = var.node_auto_scale_min
+      max     = var.node_auto_scale_max
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = var.zone
+    }
+  }
+
+  instance_template {
+    platform_id = var.node_platform_id
+
+    network_interface {
+      nat                = true
+      subnet_ids         = [yandex_vpc_subnet.momo_subnet.id]
+    }
+
+    resources {
+      memory = var.node_memory # RAM quantity in GB
+      cores  = var.node_core # Number of CPU cores
+    }
+
+    boot_disk {
+      type = var.node_disk_type
+      size = var.node_disk_size # Disk size in GB
+    }
+  }
+}
