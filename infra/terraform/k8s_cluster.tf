905448d50bb5c8747bb83476af84abd075c7c958
STARTS_Pr0 <sstartss@yandex.ru>
2024-08-31 10:22:20 +0300

    Развернуть кластер, добавить переменные

--- infra/terraform/k8s_cluster.tf
+++ infra/terraform/k8s_cluster.tf
@@ -9,7 +9,7 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
       subnet_id = yandex_vpc_subnet.momo_subnet.id
     }
   }
-   
+
   service_account_id      = yandex_iam_service_account.k8s_cluster_sa.id
   node_service_account_id = yandex_iam_service_account.k8s_cluster_sa.id
   depends_on = [
